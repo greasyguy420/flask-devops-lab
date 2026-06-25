b15f41607f26e2b7998fa067bd1cc38c1840d965
Josh <joshua@gmail.com>
2026-06-25 13:28:01 -0400

    feat: add /api/report endpoint

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import json
 import os
+import platform
+import socket
+import time
+
 from flask import Flask, jsonify, render_template_string
+
+START_TIME = time.time()
 
 app = Flask(__name__)
 
@@ -34,6 +40,13 @@ def home():
 		ver=cfg['version']
 	)
 
+@app.get('/api/report')
+def report():
+	return jsonify({
+		'hostname': socket.gethostname(),
+		'python_version': platform.python_version(),
+		'uptime_seconds': round(time.time() - START_TIME, 2)
+	})
 
 if __name__ == '__main__':
 	app.run(
